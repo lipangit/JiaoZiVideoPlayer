d671c9094c1b6a413e0c620efe058c1b3736ed17
Nathen <lian@jiecao.fm>
2015-11-27 17:04:50 +0800

    project init

---
+++ app/src/main/java/fm/jiecao/jiecaovideoplayer/MainActivity.java
@@ -0,0 +1,13 @@
+package fm.jiecao.jiecaovideoplayer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
