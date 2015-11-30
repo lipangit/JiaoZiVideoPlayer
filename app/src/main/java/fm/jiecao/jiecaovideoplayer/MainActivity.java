d1c9d04445f851a6ffb9753ac7b90887c08695d7
Nathen <lian@jiecao.fm>
2015-11-30 11:25:23 +0800

    Add lib model

--- app/src/main/java/fm/jiecao/jiecaovideoplayer/MainActivity.java
+++ app/src/main/java/fm/jiecao/jiecaovideoplayer/MainActivity.java
@@ -9,5 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
