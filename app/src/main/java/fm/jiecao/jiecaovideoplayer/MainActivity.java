b9ce2a4f8d4b07882098932aa1d9e64d46e39d87
Nathen <lipan@jiecao.fm>
2016-03-14 15:25:08 +0800

    rm log

--- app/src/main/java/fm/jiecao/jiecaovideoplayer/MainActivity.java
+++ app/src/main/java/fm/jiecao/jiecaovideoplayer/MainActivity.java
@@ -20,6 +20,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /**
+         * 如果没有埋点需求，可以不用注册eventbus
+         * <br>
+         * if you do not want to get Buried Point , you do not need regist eventbus here
+         */
         EventBus.getDefault().register(this);
 
         videoController1 = (JCVideoPlayer) findViewById(R.id.videocontroller1);
@@ -55,44 +60,36 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         JCVideoPlayer.releaseAllVideos();
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        EventBus.getDefault().unregister(this);
-    }
 
-    /**
-     * 如果没有埋点需求，可以不用注册eventbus
-     */
     public void onEventMainThread(VideoEvents event) {
         if (event.type == VideoEvents.POINT_START_ICON) {
-            Log.i("Video Event", "POINT_START_ICON");
+            Log.i("Video Event", "POINT_START_ICON" + " title is : " + event.obj + " url is : " + event.obj1);
         } else if (event.type == VideoEvents.POINT_START_THUMB) {
-            Log.i("Video Event", "POINT_START_THUMB");
+            Log.i("Video Event", "POINT_START_THUMB" + " title is : " + event.obj + " url is : " + event.obj1);
         } else if (event.type == VideoEvents.POINT_STOP) {
-            Log.i("Video Event", "POINT_STOP");
+            Log.i("Video Event", "POINT_STOP" + " title is : " + event.obj + " url is : " + event.obj1);
         } else if (event.type == VideoEvents.POINT_STOP_FULLSCREEN) {
-            Log.i("Video Event", "POINT_STOP_FULLSCREEN");
+            Log.i("Video Event", "POINT_STOP_FULLSCREEN" + " title is : " + event.obj + " url is : " + event.obj1);
         } else if (event.type == VideoEvents.POINT_RESUME) {
-            Log.i("Video Event", "POINT_RESUME");
+            Log.i("Video Event", "POINT_RESUME" + " title is : " + event.obj + " url is : " + event.obj1);
         } else if (event.type == VideoEvents.POINT_RESUME_FULLSCREEN) {
-            Log.i("Video Event", "POINT_RESUME_FULLSCREEN");
+            Log.i("Video Event", "POINT_RESUME_FULLSCREEN" + " title is : " + event.obj + " url is : " + event.obj1);
         } else if (event.type == VideoEvents.POINT_CLICK_BLANK) {
-            Log.i("Video Event", "POINT_CLICK_BLANK");
+            Log.i("Video Event", "POINT_CLICK_BLANK" + " title is : " + event.obj + " url is : " + event.obj1);
         } else if (event.type == VideoEvents.POINT_CLICK_BLANK_FULLSCREEN) {
-            Log.i("Video Event", "POINT_CLICK_BLANK_FULLSCREEN");
+            Log.i("Video Event", "POINT_CLICK_BLANK_FULLSCREEN" + " title is : " + event.obj + " url is : " + event.obj1);
         } else if (event.type == VideoEvents.POINT_CLICK_SEEKBAR) {
-            Log.i("Video Event", "POINT_CLICK_SEEKBAR");
+            Log.i("Video Event", "POINT_CLICK_SEEKBAR" + " title is : " + event.obj + " url is : " + event.obj1);
         } else if (event.type == VideoEvents.POINT_CLICK_SEEKBAR_FULLSCREEN) {
-            Log.i("Video Event", "POINT_CLICK_SEEKBAR_FULLSCREEN");
+            Log.i("Video Event", "POINT_CLICK_SEEKBAR_FULLSCREEN" + " title is : " + event.obj + " url is : " + event.obj1);
         } else if (event.type == VideoEvents.POINT_AUTO_COMPLETE) {
-            Log.i("Video Event", "POINT_AUTO_COMPLETE");
+            Log.i("Video Event", "POINT_AUTO_COMPLETE" + " title is : " + event.obj + " url is : " + event.obj1);
         } else if (event.type == VideoEvents.POINT_AUTO_COMPLETE_FULLSCREEN) {
-            Log.i("Video Event", "POINT_AUTO_COMPLETE_FULLSCREEN");
+            Log.i("Video Event", "POINT_AUTO_COMPLETE_FULLSCREEN" + " title is : " + event.obj + " url is : " + event.obj1);
         } else if (event.type == VideoEvents.POINT_ENTER_FULLSCREEN) {
-            Log.i("Video Event", "POINT_ENTER_FULLSCREEN");
+            Log.i("Video Event", "POINT_ENTER_FULLSCREEN" + " title is : " + event.obj + " url is : " + event.obj1);
         } else if (event.type == VideoEvents.POINT_QUIT_FULLSCREEN) {
-            Log.i("Video Event", "POINT_QUIT_FULLSCREEN");
+            Log.i("Video Event", "POINT_QUIT_FULLSCREEN" + " title is : " + event.obj + " url is : " + event.obj1);
         }
     }
 
@@ -115,5 +112,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 startActivity(new Intent(this, SetSkinActivity.class));
                 break;
         }
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        EventBus.getDefault().unregister(this);
     }
 }
