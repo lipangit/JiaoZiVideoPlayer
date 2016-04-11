f0388af78a616cd2ed67970f05b22922e90fd43c
Nathen <lipan@jiecao.fm>
2016-04-11 11:59:19 +0800

    add abs class

---
+++ jcvideoplayer-lib/src/main/java/fm/jiecao/jcvideoplayer_lib/AbstractJCVideoPlayer.java
@@ -0,0 +1,8 @@
+package fm.jiecao.jcvideoplayer_lib;
+
+/**
+ * Created by Nathen
+ * On 2016/04/10 15:45
+ */
+public class AbstractJCVideoPlayer {
+}
---
+++ jcvideoplayer-lib/src/main/java/fm/jiecao/jcvideoplayer_lib/JCDemoVideoPlayer.java
@@ -0,0 +1,19 @@
+package fm.jiecao.jcvideoplayer_lib;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+/**
+ * Created by Nathen
+ * On 2016/04/10 15:45
+ */
+public class JCDemoVideoPlayer extends AbstractJCVideoPlayer {
+    public JCDemoVideoPlayer(Context context) {
+        super(context);
+    }
+
+    public JCDemoVideoPlayer(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+}
