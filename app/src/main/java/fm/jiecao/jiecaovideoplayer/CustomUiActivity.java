904dc0c03b6520753f55a308273d66c6c9e90c33
Nathen <lipan@jiecao.fm>
2016-04-20 10:09:40 +0800

    class params

--- app/src/main/java/fm/jiecao/jiecaovideoplayer/CustomUiActivity.java
+++ app/src/main/java/fm/jiecao/jiecaovideoplayer/CustomUiActivity.java
@@ -7,7 +7,6 @@ import android.widget.Button;
 
 import com.nostra13.universalimageloader.core.ImageLoader;
 
-import fm.jiecao.jcvideoplayer_lib.JCAbstractVideoPlayer;
 import fm.jiecao.jcvideoplayer_lib.JCDemoVideoPlayer;
 import fm.jiecao.jcvideoplayer_lib.JCFullScreenActivity;
 import fm.jiecao.jcvideoplayer_lib.JCVideoPlayerJinRiTouTIao;
@@ -45,16 +44,14 @@ public class CustomUiActivity extends AppCompatActivity implements View.OnClickL
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.to_fullscreen_simple_demo:
-                JCAbstractVideoPlayer jcAbstractVideoPlayer = new JCDemoVideoPlayer(this);
                 JCFullScreenActivity.toActivity(this,
                         "http://2449.vod.myqcloud.com/2449_22ca37a6ea9011e5acaaf51d105342e3.f20.mp4",
-                        jcAbstractVideoPlayer.getClass(), "嫂子真浪");
+                        JCDemoVideoPlayer.class, "嫂子真浪");
                 break;
             case R.id.to_fullscreen_jinritoutiao:
-                JCAbstractVideoPlayer jCVideoPlayerJinRiTouTIao = new JCVideoPlayerJinRiTouTIao(this);
                 JCFullScreenActivity.toActivity(this,
                         "http://2449.vod.myqcloud.com/2449_22ca37a6ea9011e5acaaf51d105342e3.f20.mp4",
-                        jCVideoPlayerJinRiTouTIao.getClass(), "嫂子真牛逼");
+                        JCVideoPlayerJinRiTouTIao.class, "嫂子真牛逼");
                 break;
         }
     }
