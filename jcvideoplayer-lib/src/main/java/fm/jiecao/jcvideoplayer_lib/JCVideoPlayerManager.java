9680bd3ad4f56dc5286b7973e78d60ff2f263ddb
Nathen <lipan@jiecao.fm>
2016-07-26 01:55:41 +0800

    确定了新版本三大部分的主要工作

--- jcvideoplayer-lib/src/main/java/fm/jiecao/jcvideoplayer_lib/JCVideoPlayerManager.java
+++ jcvideoplayer-lib/src/main/java/fm/jiecao/jcvideoplayer_lib/JCVideoPlayerManager.java
@@ -1,8 +1,21 @@
 package fm.jiecao.jcvideoplayer_lib;
 
+import android.view.ViewGroup;
+
 /**
+ * Put JCVideoPlayer into layout
+ * From a JCVideoPlayer to another JCVideoPlayer
  * Created by Nathen on 16/7/26.
  */
 public class JCVideoPlayerManager {
+
+    public static void changeJCVideoPlayer(JCVideoPlayer from, JCVideoPlayer to) {
+
+    }
+
+    public static void createJCVideoPlayer(ViewGroup viewGroup, ViewGroup.LayoutParams params
+            , JCVideoPlayer jcVideoPlayer) {
+
+    }
 
 }
