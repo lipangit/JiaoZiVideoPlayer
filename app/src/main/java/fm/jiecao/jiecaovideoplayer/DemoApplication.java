960653926a6a5c057ef430a8c36a0f49e829670a
Nathen <lipan@jiecao.fm>
2016-11-24 00:22:19 +0800

    rm imageloader like

--- app/src/main/java/fm/jiecao/jiecaovideoplayer/DemoApplication.java
+++ app/src/main/java/fm/jiecao/jiecaovideoplayer/DemoApplication.java
@@ -1,17 +1,7 @@
 package fm.jiecao.jiecaovideoplayer;
 
 import android.app.Application;
-import android.graphics.Bitmap;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 
-import com.nostra13.universalimageloader.cache.disc.naming.Md5FileNameGenerator;
-import com.nostra13.universalimageloader.core.DisplayImageOptions;
-import com.nostra13.universalimageloader.core.ImageLoader;
-import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
-import com.nostra13.universalimageloader.core.assist.ImageScaleType;
-import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
-import com.nostra13.universalimageloader.core.display.FadeInBitmapDisplayer;
 import com.squareup.leakcanary.LeakCanary;
 
 /**
@@ -24,39 +14,9 @@ public class DemoApplication extends Application {
     public void onCreate() {
         super.onCreate();
         LeakCanary.install(this);
-        initUniversalImageLoader();
 
         //it is public static, you can set this everywhere
         //JCVideoPlayer.TOOL_BAR_EXIST = false;
         //JCVideoPlayer.ACTION_BAR_EXIST = false;
     }
-
-    private void initUniversalImageLoader() {
-        ImageLoaderConfiguration.Builder config = new ImageLoaderConfiguration.Builder(getApplicationContext());
-        config.threadPriority(Thread.NORM_PRIORITY - 2);
-        config.denyCacheImageMultipleSizesInMemory();
-        config.diskCacheFileNameGenerator(new Md5FileNameGenerator());
-        config.diskCacheSize(50 * 1024 * 1024); // 50 MiB
-        config.tasksProcessingOrder(QueueProcessingType.LIFO);
-        config.writeDebugLogs(); // Remove for releaseAllVideos app
-        config.defaultDisplayImageOptions(getDefaultDisplayImageOption());
-        // Initialize ImageLoader with configuration.
-        ImageLoader.getInstance().init(config.build());
-    }
-
-    public static DisplayImageOptions getDefaultDisplayImageOption() {
-        DisplayImageOptions options = new DisplayImageOptions.Builder()
-                .showImageOnLoading(new ColorDrawable(Color.parseColor("#f0f0f0")))
-                .resetViewBeforeLoading(true)
-                .cacheInMemory(true)
-                .cacheOnDisk(true)
-                .considerExifParams(true)
-                .imageScaleType(ImageScaleType.EXACTLY_STRETCHED)
-                .bitmapConfig(Bitmap.Config.RGB_565)
-                .displayer(new FadeInBitmapDisplayer(500)) // 设置图片渐显的时间
-//                .delayBeforeLoading(300)  // 下载前的延迟时间
-                .build();
-        return options;
-    }
-
 }
--- app/src/main/java/fm/jiecao/jiecaovideoplayer/UIImageLoaderActivity.java
+++ app/src/main/java/fm/jiecao/jiecaovideoplayer/UIImageLoaderActivity.java
@@ -1,19 +1,12 @@
 package fm.jiecao.jiecaovideoplayer;
 
-import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.util.LruCache;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.view.View;
 
-import com.android.volley.RequestQueue;
-import com.android.volley.toolbox.Volley;
-import com.bumptech.glide.Glide;
 import com.facebook.drawee.backends.pipeline.Fresco;
-import com.nostra13.universalimageloader.core.ImageLoader;
 import com.squareup.picasso.Picasso;
 
 import fm.jiecao.jcvideoplayer_lib.JCVideoPlayer;
@@ -23,9 +16,10 @@ import fm.jiecao.jiecaovideoplayer.CustomView.JCVideoPlayerStandardFresco;
 /**
  * Created by Nathen on 16/7/31.
  */
+@Deprecated // this should in small change
 public class UIImageLoaderActivity extends AppCompatActivity {
-    JCVideoPlayerStandard videoController1, videoController2, videoController3, videoController4;
-    JCVideoPlayerStandardFresco videoController5;
+    JCVideoPlayerStandard videoController1;
+    JCVideoPlayerStandardFresco videoController2;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -41,69 +35,53 @@ public class UIImageLoaderActivity extends AppCompatActivity {
         videoController1 = (JCVideoPlayerStandard) findViewById(R.id.videocontroller1);
         videoController1.setUp("http://video.jiecao.fm/8/17/%E6%8A%AB%E8%90%A8.mp4", JCVideoPlayer.SCREEN_LAYOUT_LIST,
                 "嫂子抓住");
-        ImageLoader.getInstance().displayImage("http://img4.jiecaojingxuan.com/2016/8/17/f2dbd12e-b1cb-4daf-aff1-8c6be2f64d1a.jpg",
-                videoController1.thumbImageView);
-
-        videoController2 = (JCVideoPlayerStandard) findViewById(R.id.videocontroller2);
-        videoController2.setUp("http://video.jiecao.fm/8/17/%E6%8A%AB%E8%90%A8.mp4", JCVideoPlayer.SCREEN_LAYOUT_LIST,
-                "嫂子别晃");
-        Glide.with(this)
-                .load("http://img4.jiecaojingxuan.com/2016/8/17/f2dbd12e-b1cb-4daf-aff1-8c6be2f64d1a.jpg")
-                .into(videoController2.thumbImageView);
-
-        videoController3 = (JCVideoPlayerStandard) findViewById(R.id.videocontroller3);
-        videoController3.setUp("http://video.jiecao.fm/8/17/%E6%8A%AB%E8%90%A8.mp4", JCVideoPlayer.SCREEN_LAYOUT_LIST,
-                "嫂子别躲");
         Picasso.with(this)
                 .load("http://img4.jiecaojingxuan.com/2016/8/17/f2dbd12e-b1cb-4daf-aff1-8c6be2f64d1a.jpg")
-                .into(videoController3.thumbImageView);
+                .into(videoController1.thumbImageView);
+//        ImageLoader.getInstance().displayImage("http://img4.jiecaojingxuan.com/2016/8/17/f2dbd12e-b1cb-4daf-aff1-8c6be2f64d1a.jpg",
+//                videoController1.thumbImageView);
 
-        videoController4 = (JCVideoPlayerStandard) findViewById(R.id.videocontroller4);
-        videoController4.setUp("http://video.jiecao.fm/8/17/%E6%8A%AB%E8%90%A8.mp4", JCVideoPlayer.SCREEN_LAYOUT_LIST,
-                "嫂子别忘了");
-        RequestQueue mQueue = Volley.newRequestQueue(getApplicationContext());
-        com.android.volley.toolbox.ImageLoader imageLoader = new com.android.volley.toolbox.ImageLoader(mQueue, new BitmapCache());
-        com.android.volley.toolbox.ImageLoader.ImageListener listener =
-                com.android.volley.toolbox.ImageLoader.getImageListener(videoController4.thumbImageView, R.mipmap.ic_launcher, R.mipmap.ic_launcher);
-        imageLoader.get("http://img4.jiecaojingxuan.com/2016/8/17/f2dbd12e-b1cb-4daf-aff1-8c6be2f64d1a.jpg", listener);
+//        Glide.with(this)
+//                .load("http://img4.jiecaojingxuan.com/2016/8/17/f2dbd12e-b1cb-4daf-aff1-8c6be2f64d1a.jpg")
+//                .into(videoController1.thumbImageView);
 
-        videoController5 = (JCVideoPlayerStandardFresco) findViewById(R.id.videocontroller5);
-        videoController5.setUp("http://video.jiecao.fm/8/17/%E6%8A%AB%E8%90%A8.mp4", JCVideoPlayer.SCREEN_LAYOUT_LIST,
+
+//        RequestQueue mQueue = Volley.newRequestQueue(getApplicationContext());
+//        com.android.volley.toolbox.ImageLoader imageLoader = new com.android.volley.toolbox.ImageLoader(mQueue, new VolleyBitmapCache());
+//        com.android.volley.toolbox.ImageLoader.ImageListener listener =
+//                com.android.volley.toolbox.ImageLoader.getImageListener(videoController1.thumbImageView, R.mipmap.ic_launcher, R.mipmap.ic_launcher);
+//        imageLoader.get("http://img4.jiecaojingxuan.com/2016/8/17/f2dbd12e-b1cb-4daf-aff1-8c6be2f64d1a.jpg", listener);
+
+        videoController2 = (JCVideoPlayerStandardFresco) findViewById(R.id.videocontroller2);
+        videoController2.setUp("http://video.jiecao.fm/8/17/%E6%8A%AB%E8%90%A8.mp4", JCVideoPlayer.SCREEN_LAYOUT_LIST,
                 "嫂子打电话");
         Uri uri = Uri.parse("http://img4.jiecaojingxuan.com/2016/8/17/f2dbd12e-b1cb-4daf-aff1-8c6be2f64d1a.jpg");
-        videoController5.thumbImageView.setImageURI(uri);
+        videoController2.thumbImageView.setImageURI(uri);
 
-        View vv = findViewById(R.id.scroll);
-        vv.setOnScrollChangeListener(new View.OnScrollChangeListener() {
-            @Override
-            public void onScrollChange(View v, int scrollX, int scrollY, int oldScrollX, int oldScrollY) {
-                videoController1.onScrollChange();
-            }
-        });
     }
 
-    public class BitmapCache implements com.android.volley.toolbox.ImageLoader.ImageCache {
-        private LruCache<String, Bitmap> cache;
-
-        public BitmapCache() {
-            cache = new LruCache<String, Bitmap>(8 * 1024 * 1024) {
-                @Override
-                protected int sizeOf(String key, Bitmap bitmap) {
-                    return bitmap.getRowBytes() * bitmap.getHeight();
-                }
-            };
-        }
-
-        @Override
-        public Bitmap getBitmap(String url) {
-            return cache.get(url);
-        }
-
-        @Override
-        public void putBitmap(String url, Bitmap bitmap) {
-            cache.put(url, bitmap);
-        }
-    }
+//    public class VolleyBitmapCache implements com.android.volley.toolbox.ImageLoader.ImageCache {
+//        private LruCache<String, Bitmap> cache;
+//
+//        public VolleyBitmapCache() {
+//            cache = new LruCache<String, Bitmap>(8 * 1024 * 1024) {
+//                @Override
+//                protected int sizeOf(String key, Bitmap bitmap) {
+//                    return bitmap.getRowBytes() * bitmap.getHeight();
+//                }
+//            };
+//        }
+//
+//        @Override
+//        public Bitmap getBitmap(String url) {
+//            return cache.get(url);
+//        }
+//
+//        @Override
+//        public void putBitmap(String url, Bitmap bitmap) {
+//            cache.put(url, bitmap);
+//        }
+//    }
 
     @Override
     public void onBackPressed() {
