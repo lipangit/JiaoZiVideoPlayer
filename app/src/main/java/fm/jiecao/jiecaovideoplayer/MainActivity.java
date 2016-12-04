13eec3855d27ccea52e07cbe2aaa9faa303552b1
Nathen <lipan@jiecao.fm>
2016-12-04 16:36:21 +0800

    local path eg

--- app/src/main/java/fm/jiecao/jiecaovideoplayer/MainActivity.java
+++ app/src/main/java/fm/jiecao/jiecaovideoplayer/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -11,6 +12,11 @@ import android.view.View;
 import android.widget.Button;
 
 import com.squareup.picasso.Picasso;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 import fm.jiecao.jcvideoplayer_lib.JCUserAction;
 import fm.jiecao.jcvideoplayer_lib.JCUserActionStandard;
@@ -54,10 +60,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mJcVideoPlayerSimple = (JCVideoPlayerSimple) findViewById(R.id.simple_demo);
         mJcVideoPlayerSimple.setUp("http://devimages.apple.com/iphone/samples/bipbop/gear1/prog_index.m3u8"
                 , JCVideoPlayerStandard.SCREEN_LAYOUT_NORMAL, "嫂子在家吗");
-
         mJcVideoPlayerStandard = (JCVideoPlayerStandard) findViewById(R.id.jc_video);
-        mJcVideoPlayerStandard.setUp("http://video.jiecao.fm/11/23/xin/%E5%81%87%E4%BA%BA.mp4"
-                , JCVideoPlayerStandard.SCREEN_LAYOUT_NORMAL, "嫂子不信");
+
+//        mJcVideoPlayerStandard.setUp("http://video.jiecao.fm/11/23/xin/%E5%81%87%E4%BA%BA.mp4"
+//                , JCVideoPlayerStandard.SCREEN_LAYOUT_NORMAL, "嫂子不信");
+
+        /** Play video in local path, eg:record by system camera **/
+//        cpAssertVideoToLocalPath();
+//        mJcVideoPlayerStandard.setUp(Environment.getExternalStorageDirectory().getAbsolutePath() + "/DCIM/Camera/local_video.mp4"
+//                , JCVideoPlayerStandard.SCREEN_LAYOUT_NORMAL, "嫂子不信");
+
+        /** Play video in assert **/
+//        mJcVideoPlayerStandard.setUp("file:///android_asset/local_video.mp4"
+//                , JCVideoPlayerStandard.SCREEN_LAYOUT_NORMAL, "嫂子不信");
+
         Picasso.with(this)
                 .load("http://img4.jiecaojingxuan.com/2016/11/23/00b026e7-b830-4994-bc87-38f4033806a6.jpg@!640_360")
                 .into(mJcVideoPlayerStandard.thumbImageView);
@@ -169,6 +185,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     Log.i("USER_EVENT", "unknow");
                     break;
             }
+        }
+    }
+
+    public void cpAssertVideoToLocalPath() {
+        try {
+            InputStream myInput;
+            OutputStream myOutput = new FileOutputStream(Environment.getExternalStorageDirectory().getAbsolutePath() + "/DCIM/Camera/local_video.mp4");
+            myInput = this.getAssets().open("local_video.mp4");
+            byte[] buffer = new byte[1024];
+            int length = myInput.read(buffer);
+            while (length > 0) {
+                myOutput.write(buffer, 0, length);
+                length = myInput.read(buffer);
+            }
+
+            myOutput.flush();
+            myInput.close();
+            myOutput.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
