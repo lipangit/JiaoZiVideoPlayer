125a343aa94c3e1c7a13cbac3e3e64f8125644aa
Nathen <lipan@jiecao.fm>
2016-12-30 21:29:16 +0800

    orientation demo

---
+++ app/src/main/java/fm/jiecao/jiecaovideoplayer/OrientationActivity.java
@@ -0,0 +1,10 @@
+package fm.jiecao.jiecaovideoplayer;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Nathen on 2016/12/30.
+ */
+
+public class OrientationActivity extends AppCompatActivity {
+}
