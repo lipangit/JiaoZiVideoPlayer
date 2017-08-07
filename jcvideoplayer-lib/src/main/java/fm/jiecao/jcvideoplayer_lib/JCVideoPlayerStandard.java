6ad3b1ad56a0cd420dbf3b4274afdbb35d373e5c
Nathen <lipan@jiecao.fm>
2017-08-07 23:47:53 +0800

    fix code

--- jcvideoplayer-lib/src/main/java/fm/jiecao/jcvideoplayer_lib/JCVideoPlayerStandard.java
+++ jcvideoplayer-lib/src/main/java/fm/jiecao/jcvideoplayer_lib/JCVideoPlayerStandard.java
@@ -695,7 +695,7 @@ public class JCVideoPlayerStandard extends JCVideoPlayer {
     public void setAllControlsVisible(int topCon, int bottomCon, int startBtn, int loadingPro,
                                       int thumbImg, int coverImg, int bottomPro) {
         //TODO 这个地方由于前边的各种状态不是太明白，所以暂时只能这样写一下（目前没发现问题），作者可以优化一下
-        if (!isVideoRendingStart && currentScreen != SCREEN_WINDOW_FULLSCREEN && currentScreen != SCREEN_WINDOW_TINY) {
+        if (!isVideoRendingStart) {
             //只要没开始播放，一直显示缩略图
             thumbImg = VISIBLE;
         }
