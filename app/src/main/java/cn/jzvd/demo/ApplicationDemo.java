9e180914df8c031e62cd7519865047b5aa272d0d
Nathen <821537155@qq.com>
2018-01-18 21:40:44 +0800

    video  cache

--- app/src/main/java/cn/jzvd/demo/ApplicationDemo.java
+++ app/src/main/java/cn/jzvd/demo/ApplicationDemo.java
@@ -1,7 +1,9 @@
 package cn.jzvd.demo;
 
 import android.app.Application;
+import android.content.Context;
 
+import com.danikula.videocache.HttpProxyCacheServer;
 import com.squareup.leakcanary.LeakCanary;
 
 /**
@@ -18,5 +20,16 @@ public class ApplicationDemo extends Application {
         //it is public static, you can set this everywhere
         //JZVideoPlayer.TOOL_BAR_EXIST = false;
         //JZVideoPlayer.ACTION_BAR_EXIST = false;
+    }
+
+    private HttpProxyCacheServer proxy;
+
+    public static HttpProxyCacheServer getProxy(Context context) {
+        ApplicationDemo app = (ApplicationDemo) context.getApplicationContext();
+        return app.proxy == null ? (app.proxy = app.newProxy()) : app.proxy;
+    }
+
+    private HttpProxyCacheServer newProxy() {
+        return new HttpProxyCacheServer(this);
     }
 }
