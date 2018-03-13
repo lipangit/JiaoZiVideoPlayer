fb3237e32e8e844105aff8be1b3d918ba575ee6b
Wangshiwei <itshiwei@aliyun.com>
2018-03-13 15:45:26 +0800

    参照主干，还原

--- app/src/main/java/cn/jzvd/demo/ActivityListViewRecyclerView.java
+++ app/src/main/java/cn/jzvd/demo/ActivityListViewRecyclerView.java
@@ -53,7 +53,6 @@ public class ActivityListViewRecyclerView extends AppCompatActivity {
                 }
             }
         });
-        recyclerView.addOnScrollListener(new FeedScrollListener());
     }
 
     @Override
@@ -78,126 +77,6 @@ public class ActivityListViewRecyclerView extends AppCompatActivity {
                 break;
         }
         return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * 监听recycleView滑动状态，
-     * 自动播放可见区域内的视频
-     */
-    private static class FeedScrollListener extends RecyclerView.OnScrollListener {
-
-        private int firstVisibleItem = 0;
-        private int lastVisibleItem = 0;
-        private int visibleCount = 0;
-
-        /**
-         * 视频状态标签
-         */
-        private enum VideoTagEnum {
-
-            /**
-             * 自动播放视频
-             */
-            TAG_AUTO_PLAY_VIDEO,
-
-            /**
-             * 暂停视频
-             */
-            TAG_PAUSE_VIDEO
-        }
-
-
-        @Override
-        public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
-            super.onScrollStateChanged(recyclerView, newState);
-
-            // TODO 这可以加上 wifi 下自动播放视频开关
-//            if (wifiTag) {
-            switch (newState) {
-                case RecyclerView.SCROLL_STATE_IDLE:
-                    autoPlayVideo(recyclerView, VideoTagEnum.TAG_AUTO_PLAY_VIDEO);
-                default:
-                    // 滑动时暂停视频
-                    //  autoPlayVideo(recyclerView, VideoTagEnum.TAG_PAUSE_VIDEO);
-                    break;
-            }
-//            } else {
-//              // do some ....
-//            }
-        }
-
-        @Override
-        public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
-            super.onScrolled(recyclerView, dx, dy);
-
-            RecyclerView.LayoutManager layoutManager = recyclerView.getLayoutManager();
-
-            if (layoutManager instanceof LinearLayoutManager) {
-
-                LinearLayoutManager linearManager = (LinearLayoutManager) layoutManager;
-
-                firstVisibleItem = linearManager.findFirstVisibleItemPosition();
-                lastVisibleItem = linearManager.findLastVisibleItemPosition();
-                visibleCount = lastVisibleItem - firstVisibleItem
-                //TODO  + headerView 的个数 ?
-                ;
-            }
-
-        }
-
-        /**
-         * 循环遍历 可见区域的播放器
-         * 然后通过 getLocalVisibleRect(rect)方法计算出哪个播放器完全显示出来
-         * <p>
-         * getLocalVisibleRect相关链接：http://www.cnblogs.com/ai-developers/p/4413585.html
-         *
-         * @param view
-         * @param handleVideoTag 视频需要进行状态
-         */
-        private void autoPlayVideo(RecyclerView view, VideoTagEnum handleVideoTag) {
-            for (int i = 0; i < visibleCount; i++) {
-                if (view != null && view.getChildAt(i) != null && view.getChildAt(i).findViewById(R.id.videoplayer) != null) {
-                    JZVideoPlayerStandard homeGSYVideoPlayer = (JZVideoPlayerStandard) view.getChildAt(i).findViewById(R.id.videoplayer);
-
-                    Rect rect = new Rect();
-                    homeGSYVideoPlayer.getLocalVisibleRect(rect);
-                    int videoheight = homeGSYVideoPlayer.getHeight();
-                    if (rect.top == 0 && rect.bottom == videoheight) {
-                        handleVideo(handleVideoTag, homeGSYVideoPlayer);
-                        // 借助跳出循环，达到只处理可见区域内的第一个播放器
-                        break;
-                    }
-                }
-            }
-
-        }
-
-        /**
-         * 视频状态处理
-         *
-         * @param handleVideoTag     视频需要进行状态
-         * @param homeGSYVideoPlayer JZVideoPlayer播放器
-         */
-        private void handleVideo(VideoTagEnum handleVideoTag, JZVideoPlayerStandard homeGSYVideoPlayer) {
-            switch (handleVideoTag) {
-                case TAG_AUTO_PLAY_VIDEO:
-                    if ((homeGSYVideoPlayer.currentState != JZVideoPlayerStandard.CURRENT_STATE_PLAYING)) {
-                        // 进行播放
-                        homeGSYVideoPlayer.startVideo();
-                    }
-                    break;
-                case TAG_PAUSE_VIDEO:
-                    if ((homeGSYVideoPlayer.currentState != JZVideoPlayerStandard.CURRENT_STATE_PAUSE)) {
-                        // 模拟点击播放Button,实现暂停视频
-                        homeGSYVideoPlayer.startButton.performClick();
-                    }
-                    break;
-                default:
-                    break;
-            }
-        }
-
-
     }
 
 }
