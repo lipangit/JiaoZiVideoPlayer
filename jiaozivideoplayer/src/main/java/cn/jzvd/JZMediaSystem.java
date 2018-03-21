3af8166e8156cb2b24abc5509dd2bf734f6e1768
Nathen <821537155@qq.com>
2018-03-21 19:06:52 +0800

    fix bug on some phone

--- jiaozivideoplayer/src/main/java/cn/jzvd/JZMediaSystem.java
+++ jiaozivideoplayer/src/main/java/cn/jzvd/JZMediaSystem.java
@@ -173,7 +173,9 @@ public class JZMediaSystem extends JZMediaInterface implements MediaPlayer.OnPre
             public void run() {
                 if (JZVideoPlayerManager.getCurrentJzvd() != null) {
                     if (what == MediaPlayer.MEDIA_INFO_VIDEO_RENDERING_START) {
-                        JZVideoPlayerManager.getCurrentJzvd().onPrepared();
+                        if (JZVideoPlayerManager.getCurrentJzvd().currentState == JZVideoPlayer.CURRENT_STATE_PREPARING) {
+                            JZVideoPlayerManager.getCurrentJzvd().onPrepared();
+                        }
                     } else {
                         JZVideoPlayerManager.getCurrentJzvd().onInfo(what, extra);
                     }
